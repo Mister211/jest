4f38ec0243e45e12a53007e415a10b991ca9f6f0
Mister211 <jljinete@uninorte.edu.co>
2024-05-04 08:11:42 -0500

    todo

---
+++ app/utils/division.test.ts
@@ -0,0 +1,9 @@
+import { division } from "./division";
+Run|Debug
+test("Test", () => {
+  expect(division(10, 2)).toBe(5);
+});
+Run|Debug
+test("Test", () =>{
+    expect(division(1, 0).tobe("El divisor debe ser diferente a cero papa bello"));
+});
---
+++ app/utils/division.ts
@@ -0,0 +1,6 @@
+export function division(a: number, b: number) {
+  if (b == 0) {
+    return " el valor debe ser diferente a cero"
+  }
+  return a/b
+}
